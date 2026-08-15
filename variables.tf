be95f0dc21a643f64be4e7b36da25ee2a68ee388
gio1505 <30695165+gio1505@users.noreply.github.com>
2026-08-15 15:36:24 +0400

    ts

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
 #variable "instance_type" {
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
